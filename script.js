0dcf9e22bf11f59dbb1934522119f6474450d63f
Rkunski <radek.kunski@gmail.com>
2021-09-20 19:06:05 +0300

    finish

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 var tab = document.getElementById("pirmas");
 tab.style.display= "flex";
 function changeHeader(x, name){
-  var tabs =document.getElementsByClassName("tab");
+  var tabs = document.getElementsByClassName("tab");
   var buttons = document.getElementsByTagName("button");
   for (var i = 0; i < tabs.length; i++) {
     tabs[i].style.display = "none";
-    buttons[i].classList.remove("active");
+    // buttons[i].classList.remove("active");
   }
 var tab = document.getElementById(name);
 tab.style.display= "flex";
