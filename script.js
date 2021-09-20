6d057ccb7421c7b1676bbc21eecc8edae497ce8e
Rkunski <radek.kunski@gmail.com>
2021-09-20 12:51:07 +0300

    last

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ var tab = document.getElementById(name);
 tab.style.display= "flex";
 x.currentTarget.classList.add("active");
 }
+
+var burger = document.getElementById("burger");
+var sideMenu = document.getElementById("side-menu");
+burger.addEventListener("click",openSideMenu);
+function openSideMenu(){
+  sideMenu.classList.toggle("translateX");
+  burger.classList.toggle("rotate");
+}
+var sideLinks = document.getElementsByClassName("sideLinks");
+for (var i = 0; i < sideLinks.length; i++) {
+  sideLinks[i].addEventListener("click", openSideMenu);
+}
+ var topLinks = document.getElementsByClassName("topLinks");
+ for (var i = 0; i < topLinks.length; i++) {
+   topLinks[i].addEventListener("click",clicked);
+   function clicked(){
+     for (var i = 0; i < topLinks.length; i++) {
+       topLinks[i].classList.remove("activex");
+     }
+     this.classList.add("activex");
+   }
+ }
